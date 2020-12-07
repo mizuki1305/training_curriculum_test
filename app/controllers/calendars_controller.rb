eab64cf50bce31230f6f31319e26348cd80d4a7d
Hiroki Seike <sakaki.mizuki.13059029@gmail.com>
2020-12-07 19:58:20 +0900

    修正2

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
   def get_week   # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
